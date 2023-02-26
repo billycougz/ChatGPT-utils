38dfbca70b68f782b89e04454014fa9498dc0827
William Cougan <william.cougan@gmail.com>
2023-02-26 11:37:18 -0500

    Update overrideConsoleErrors.js

--- overrideConsoleErrors.js
+++ overrideConsoleErrors.js
@@ -1,3 +1,4 @@
+// Write me a JavaScript utility function that accepts an array of error messages as a param and overrides the global console.error function to ignore all errors provided in the array
 function overrideConsoleErrors(messagesToIgnore) {
   const originalConsoleError = console.error;
   console.error = function() {
